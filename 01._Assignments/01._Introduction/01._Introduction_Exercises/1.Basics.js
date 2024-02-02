276d5111ecd66fabb0447a0ee2ca136e454e4ea5
Jacobtkristensen <115540527+Jacobtkristensen@users.noreply.github.com>
2024-02-02 10:25:14 +0100

    1.Basics.js elaborated

--- 01._Assignments/01._Introduction/01._Introduction_Exercises/1.Basics.js
+++ 01._Assignments/01._Introduction/01._Introduction_Exercises/1.Basics.js
@@ -8,17 +8,24 @@ const lastName = "Latif";
 // EXERCISE
 // show in the console
 // My first name is Anders and my last name is Latif
-console.log(`My first name is ${firstName} and my last name is ${lastName}`)
+const fullName = `My first name is ${firstName} and my last name is ${lastName}`
+console.log(fullName)
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
 
 const year = "2023";
 const increment = 1;
-
 // Add the year plus the increment
 // The result should be 2024
 // You cannot touch line 1 or 2
-console.log(parseInt(year) + increment);
 
+let newYear = parseInt(year) + increment;
+// let newYear = Number(year) + increment;
+// let newYear = +year + increment;
+console.log(newYear);
+
+
+console.log(Number("1231af1231")) //NaN
+console.log(parseInt("1231af1231")) //1231
 // --------------------------------------
