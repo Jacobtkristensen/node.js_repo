c5db960f0b5e5f7dc8929d32a9b2056e64d97bf5
Jacob Kristensen <jacobtkristensen@gmail.com>
2024-02-15 23:20:36 +0100

    Drinks API done

--- 01._Assignments/02._Drink_API/app.js
+++ 01._Assignments/02._Drink_API/app.js
@@ -3,37 +3,60 @@ const app = require("express")();
 const drinks = [
   {
     id: 1,
-    name: "Coca Cola",
-    price: 2.5,
+    name: "Coca-Cola",
+    price: 12.5,
     alcohol: false,
   },
   {
     id: 2,
-    name: "Beer",
-    price: 3.5,
+    name: "Heineken",
+    price: 19.95,
     alcohol: true,
   },
   {
     id: 3,
-    name: "Water",
-    price: 1.5,
+    name: "Tap water",
+    price: 3.5,
     alcohol: false,
+  },
+  {
+    id: 4,
+    name: "Red wine",
+    price: 69.69,
+    alcohol: true,
+  },
+  {
+    id: 5,
+    name: "White wine",
+    price: 29.99,
+    alcohol: true,
   },
 ];
 
 app.get("/drinks", (req, res) => {
-    res.send(drinks);
-    });
-
-app.get("/drinks/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const drink = drinks.find((drink) => drink.id === id);
-    if (drink) {
-        res.send(drink);
-    } else {
-        res.send({ data: "Drink not found" });
-    }
+  res.send(drinks);
 });
 
+app.get("/drinks/alcohol", (req, res) => {
+  const drinksAlcohol = drinks.filter((drink) => drink.alcohol === true);
+  res.send(drinksAlcohol);
+}
+);
+
+app.get("/drinks/non-alcohol", (req, res) => {
+  const drinksNonAlcohol = drinks.filter((drink) => drink.alcohol === false);
+  res.send(drinksNonAlcohol);
+}
+);
+
+app.get("/drinks/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const drink = drinks.find((drink) => drink.id === id);
+  if (drink) {
+    res.send(drink);
+  } else {
+    res.send({ data: "Drink not found" });
+  }
+});
 
 app.listen(8080);
