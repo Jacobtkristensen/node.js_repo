c04e1dbeb2004f2fbe991ff7c5477378e31a6da5
Jacob Kristensen <jacobtkristensen@gmail.com>
2024-02-29 18:04:21 +0100

    Time app WIP. simple stopwatch & changing of page colors depending on time of day

---
+++ 01._Assignments/05._Time/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+console.log(__dirname)
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/home.html");
+});
+
+
+
+const PORT = 8080;
+
+app.listen(PORT, () => console.log("Server is running on port", PORT));
