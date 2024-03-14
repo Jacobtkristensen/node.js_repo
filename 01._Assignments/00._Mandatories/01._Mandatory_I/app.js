2cf3deebd56e7eaf6b8080c4f7b8d72b392c07fe
Jacob Kristensen <jacobtkristensen@gmail.com>
2024-03-14 22:00:45 +0100

    Mandatory I setup

---
+++ 01._Assignments/00._Mandatories/01._Mandatory_I/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("public"));
+
+import getMatches from "./util/matches.js";
+
+
+import { homepagePage, matchesPage, contactPage } from "./util/readPages.js";
+
+
+// ========= HTML =================
+
+app.get("/", (req, res) => {
+    res.send(homepagePage);
+});
+
+app.get("/matches", (req, res) => {
+    res.send(matchesPage);
+});
+
+app.get("/contact", (req, res) => {
+    res.send(contactPage);
+});
+
+// ========== API =================
+
+app.get("/api/matches", async (req, res) => {
+    const matches = await getMatches();
+    res.send({ data: matches });
+});
+
+const PORT = 8080;
+app.listen(PORT, () => console.log("Server is running on port", PORT));
