7ae2dd1c52f799cfb84b3d58781e041d64be8d6e
Jacobtkristensen <115540527+Jacobtkristensen@users.noreply.github.com>
2024-03-15 10:09:11 +0100

    asynchronous.js added

--- 04._DogInder/util/templatingEngine.js
+++ 04._DogInder/util/templatingEngine.js
@@ -4,12 +4,18 @@ export function readPage(path) {
   return fs.readFileSync(path).toString();
 }
 
+// //nullish coalesce operator ?? ||
+// const value = true;
+
+// console.log(value ?? "some other value");
+// console.log(value || "some other value"); // type coerces... takes falsy values as well
+
 const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
 
 export function renderPage(page, config={}) {
     const header = fs.readFileSync("./public/components/header/header.html").toString();
-    return header.replace("$TAB_TITLE$", config.tabTitle || "DogInder")
-                .replace("$CSS_LINKS$", config.CSSLinks || "")
+    return header.replace("$TAB_TITLE$", config.tabTitle ?? "DogInder")
+                .replace("$CSS_LINKS$", config.CSSLinks ?? "")
   + page 
   + footer;
 }
