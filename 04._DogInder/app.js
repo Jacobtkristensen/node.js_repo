e6d9a4c46a2353a5cd925583834497f8f49e078c
Jacobtkristensen <115540527+Jacobtkristensen@users.noreply.github.com>
2024-03-15 16:40:09 +0100

    router revamp

--- 04._DogInder/app.js
+++ 04._DogInder/app.js
@@ -4,32 +4,13 @@ const app = express();
 
 app.use(express.static("public"));
 
-import getMatches from "./util/matches.js";
+import matchesRouter from "./routers/matchesRouter.js"
+app.use(matchesRouter);
+import pagesRouter from "./routers/pagesRouter.js"
+app.use(pagesRouter);
 
 
-import { homepagePage, matchesPage, contactPage } from "./util/readPages.js";
 
-
-// ========= HTML =================
-
-app.get("/", (req, res) => {
-    res.send(homepagePage);
-});
-
-app.get("/matches", (req, res) => {
-    res.send(matchesPage);
-});
-
-app.get("/contact", (req, res) => {
-    res.send(contactPage);
-});
-
-// ========== API =================
-
-app.get("/api/matches", async (req, res) => {
-    const matches = await getMatches();
-    res.send({ data: matches });
-});
 
 const PORT = 8080;
 app.listen(PORT, () => console.log("Server is running on port", PORT));
