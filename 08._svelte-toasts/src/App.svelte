de33a50036ca2b620b6a5df679bbea09a30c0a51
Jacob Kristensen <jacobtkristensen@gmail.com>
2024-04-11 22:07:20 +0200

    toasts

---
+++ 08._svelte-toasts/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import toast, {Toaster} from 'svelte-french-toast'
+
+	function handleClick() {
+		toast.success('Hello, world!')
+	}
+
+  function helloDarkness() {
+    toast('Hello Darkness', {
+      
+    })
+  }
+</script>
+
+<Toaster />
+<button type="button" on:click={handleClick}>Toast</button>
+
+<button type="button" on:click={helloDarkness}>Darkness</button>
